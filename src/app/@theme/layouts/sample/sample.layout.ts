20d045e5b9d5bb317368666f40f7bb27830f73c9
Theo <theo.pierret1@gmail.com>
2018-05-31 11:38:09 +0200

    Add reset button

--- src/app/@theme/layouts/sample/sample.layout.ts
+++ src/app/@theme/layouts/sample/sample.layout.ts
@@ -12,43 +12,17 @@ import {
   NbThemeService,
 } from '@nebular/theme';
 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { CreateRuleComponent } from '../../../Components/Modals/CreateRule/createRule.component';
+
 import { StateService } from '../../../@core/data/state.service';
 
 // TODO: move layouts into the framework
 @Component({
   selector: 'ngx-sample-layout',
   styleUrls: ['./sample.layout.scss'],
-  template: `
-    <nb-layout [center]="layout.id === 'center-column'" windowMode>
-      <nb-layout-header fixed>
-        <ngx-header [position]="sidebar.id === 'start' ? 'normal': 'inverse'"></ngx-header>
-      </nb-layout-header>
-
-      <nb-sidebar class="menu-sidebar"
-                   tag="menu-sidebar"
-                   responsive
-                   [end]="sidebar.id === 'end'">
-        <nb-sidebar-header>
-          <a href="https://github.com/Globuss/cellular-automaton" class="btn btn-hero-success main-btn">
-            <i class="ion ion-social-github"></i> <span>View project</span>
-          </a>
-        </nb-sidebar-header>
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
-
-      <nb-layout-column class="main-content">
-        <ng-content select="router-outlet"></ng-content>
-      </nb-layout-column>
-
-      <nb-sidebar class="settings-sidebar"
-                   tag="settings-sidebar"
-                   state="collapsed"
-                   fixed
-                   [end]="sidebar.id !== 'end'">
-        <ngx-theme-settings></ngx-theme-settings>
-      </nb-sidebar>
-    </nb-layout>
-  `,
+  templateUrl: 'sample.layout.html',
 })
 
 export class SampleLayoutComponent implements OnDestroy {
