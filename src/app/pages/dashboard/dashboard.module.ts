7fe42b04fd1583cfaa213912ba8a42d869f6ab20
Theo <theo.pierret1@gmail.com>
2018-06-05 13:54:47 +0200

    ModalFiller + ModalSettings = IMPORT IN THE MODULE !!

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { ToasterModule } from 'angular2-toaster';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { CreateRuleComponent } from './../../Components/Modals/CreateRule/createRule.component';
+import { CreateFillerComponent } from './../../Components/Modals/CreateFiller/createFiller.component';
+import { SettingsComponent } from './../../Components/Modals/Settings/settings.component';
 
 @NgModule({
   imports: [
@@ -17,9 +19,13 @@ import { CreateRuleComponent } from './../../Components/Modals/CreateRule/create
   declarations: [
     DashboardComponent,
     CreateRuleComponent,
+    CreateFillerComponent,
+    SettingsComponent,
   ],
   entryComponents: [
-    CreateRuleComponent
+    CreateRuleComponent,
+    CreateFillerComponent,
+    SettingsComponent,
   ],
 })
 export class DashboardModule { }
